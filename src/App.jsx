249ebc6d882a9723a8efcf8c20bd0756cfc7bebf
Aitor <aitor.delacueva13@gmail.com>
2023-10-16 20:51:14 +0200

    Implementar Contador, Selector y comienzo de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Contador from './components/Contador';
+import Selector from './components/Selector';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedDish, setSelectedDish] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const handleSelection = (option) => {
+    const isOptionSelected = selectedDish.some((item) => item.id === option.id);
+
+    let updatedSelections;
+
+    if (isOptionSelected) {
+      // Si el producto está siendo deseleccionado, restamos su precio del total
+      updatedSelections = selectedDish.filter((item) => item.id !== option.id);
+      const newTotalPrice = totalPrice - option.price;
+      setTotalPrice(newTotalPrice);
+    } else {
+      // Si el producto está siendo seleccionado, sumamos su precio al total
+      updatedSelections = [...selectedDish, option];
+      const newTotalPrice = totalPrice + option.price;
+      setTotalPrice(newTotalPrice);
+    }
+
+    setSelectedDish(updatedSelections);
+  };
+
+  const firstDish = [
+    { id: 1, name: 'Ensalada Caprese', price: 2 },
+    { id: 2, name: 'Sopa de Tomate', price: 2 },
+    { id: 3, name: 'Pasta Carbonara', price: 2 },
+    { id: 4, name: 'Gazpacho', price: 2 },
+    { id: 5, name: 'Bruschetta', price: 2 },
+  ];
+
+  const secondDish = [
+    { id: 1, name: 'Paella', price: 2 },
+    { id: 2, name: 'Lasaña', price: 2 },
+    { id: 3, name: 'Pescado a la Parrilla', price: 2 },
+    { id: 4, name: 'Pollo al Curry', price: 2 },
+    { id: 5, name: 'Tacos de Carnitas', price: 2 },
+  ];
+
+  const dessert = [
+    { id: 1, name: 'Tiramisú', price: 2 },
+    { id: 2, name: 'Flan', price: 2 },
+    { id: 3, name: 'Pastel de Chocolate', price: 2 },
+    { id: 4, name: 'Crepes con Nutella y Fresas', price: 2 },
+    { id: 5, name: 'Helado de Vainilla con Frutas', price: 2 },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Menú</h1>
+      <Selector
+        title='Primer plato'
+        options={firstDish}
+        handleSelection={handleSelection}
+      />
+      <Selector
+        title='Segundo plato'
+        options={secondDish}
+        handleSelection={handleSelection}
+      />
+      <Selector
+        title='Postre'
+        options={dessert}
+        handleSelection={handleSelection}
+      />
+
+      <Contador total={totalPrice} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Contador.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contador = ({ total }) => {
+  return <div>Total:€{total} </div>;
+};
+
+export default Contador;
